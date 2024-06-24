5427d8be3a6b2951ef1528708c923ffe16bd393f
sariya <dayzwaitik123@yandex.ru>
2024-06-24 10:28:07 +0300

    Add share by email

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
+    path("", views.PostListView.as_view(), name="post_list"),
     path("<int:year>/<int:month>/<int:day>/<slug:post_slug>/", views.post_detail, name="post_detail"),
+    path("<int:post_pk>/share/", views.post_share, name="post_share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,35 @@
 from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 1
+    template_name = "blog/post/list.html"
 
 
-def post_list(request: HttpRequest) -> HttpResponse:
-    posts_list = Post.published.all()
-    paginator = Paginator(posts_list, 1)
-    page_number = request.GET.get("page", 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+def post_share(request: HttpRequest, post_pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_pk, status=Post.Status.PUBLISHED)
+    sent = False
 
-    return render(request, "blog/post/list.html", {"posts": posts})
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{clean_data['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {clean_data['name']} comments: {clean_data['comment']}"
+            send_mail(subject, message, "account@gmail.com", [clean_data["recipient"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, "blog/post/share.html", {"post": post, "form": form, "sent": sent})
 
 
 def post_detail(request: HttpRequest, year: int, month: int, day: int, post_slug: str) -> HttpResponse:
