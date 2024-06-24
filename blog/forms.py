19e573dc0cef029a2638f1953fa18ac8bb79cade
sariya <dayzwaitik123@yandex.ru>
2024-06-24 19:18:48 +0300

    Translate

--- blog/forms.py
+++ blog/forms.py
@@ -5,16 +5,20 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=50)
+    name = forms.CharField(max_length=50, label="Ник")
     email = forms.EmailField()
-    recipient = forms.EmailField()
-    comment = forms.CharField(required=False, widget=forms.Textarea)
+    recipient = forms.EmailField(label="Кому")
+    comment = forms.CharField(required=False, widget=forms.Textarea, label="Сообщение")
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["name", "email", "content"]
+        labels = {
+            "name": "Ник",
+            "content": "Комментарий"
+        }
 
 
 class SearchForm(forms.Form):
