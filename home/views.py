c73983b06ae297bb87966b0a032a03b02a4fd7dd
sariya <dayzwaitik123@yandex.ru>
2024-06-25 22:48:15 +0300

    redirect

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 def index(request):
-    return render(request, "home/index.html")
+    return redirect("blog:post_list")
